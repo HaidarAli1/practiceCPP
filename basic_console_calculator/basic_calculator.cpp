c5fe8802f3189e3f36e3cee9c91ca13f02c86319
Haidar Ali <shehrihaidarali@gmail.com>
2022-09-09 11:22:47 -0400

    Added console_checkrs and some optimzation to basic_console_calculators.

--- basic_console_calculator/basic_calculator.cpp
+++ basic_console_calculator/basic_calculator.cpp
@@ -1,53 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int t = 1;
+
 
 int main ()
 {
-    
-    while (t == 1) 
-    {
+    int i = 1;
+
+    struct Variables {
         int x;
-        cout << "What do you want the first number to be?" << endl;
-        cin >> x;
-
         int y;
-        cout << "What do you want the second number to be?" << endl;
-        cin >> y;
-
         char op;
-        cout << "Operator +,-,*,/" << endl;
-        cin >> op;
-
-        double total;
-
-        if (op == '+')
-        {
-            total = x + y;
-        }else if (op == '-')
-        {
-            total = x - y;
-        }else if (op == '*')
-        {
-            total = x * y;
-        }else if (op == '/')
-        {
-            total = x / y;
-        }
-
-        cout << total << endl;
-
         char restart;
-        cout << "Would you like to restart the program? y/n" << endl;
+        double total;
+    } inputVar;
 
-        if (restart == 'n' || restart == 'N')
+    while (i == 1) 
+    {
+        cout << "What do you want the first number to be?" << endl;
+        cin >> inputVar.x;
+
+        cout << "What do you want the second number to be?" << endl;
+        cin >> inputVar.y;
+
+        cout << "Operator +,-,*,/" << endl;
+        cin >> inputVar.op;
+
+        if (inputVar.op == '+')
         {
-            t = 0;
+            inputVar.total = inputVar.x + inputVar.y;
+        }else if (inputVar.op == '-')
+        {
+            inputVar.total = inputVar.x - inputVar.y;
+        }else if (inputVar.op == '*')
+        {
+            inputVar.total = inputVar.x * inputVar.y;
+        }else if (inputVar.op == '/')
+        {
+            inputVar.total = inputVar.x / inputVar.y;
         }
+
+        cout << inputVar.total << endl;
+
+        cout << "Would you like to restart the program? y/n" << endl;
+        cin >> inputVar.restart;
+
+        if (inputVar.restart == 'n' || inputVar.restart == 'N'){return 0;}
         cin.clear();
     }
-    return 0;
-    
-    
 }
---
+++ console_checkers/consolecheckers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class pawn {
+    bool color;
+    int x;
+    int y;
+    
+}
+
+
+int main () {
+
+}
